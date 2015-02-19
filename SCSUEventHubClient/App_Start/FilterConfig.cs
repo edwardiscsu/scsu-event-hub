49955e818793da83097a298d47bcaa1e50cbee26
edwardiscsu <ised03001@stcloudstate.edu>
2015-02-19 12:47:45 -0600

    Added initial Web Client Project

---
+++ SCSUEventHubClient/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace SCSUEventHubClient
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ SCSUEventHubClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SCSUEventHubClient.MvcApplication" Language="C#" %>
