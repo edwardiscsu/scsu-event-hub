12ca084dd6c440605984b797e264397e067eeec6
Harlan Sang <harlansang@gmail.com>
2015-03-30 11:31:04 -0500

    Initial Test Commit Harlan

--- SCSUEventHubModels/Models/Category.cs
+++ SCSUEventHubModels/Models/Category.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace SCSUEventHubModels.Models
 {
     public class Category
     {
+        public Category() { }
+
+        [Key]
+        public int CategoryId { get; set; }
+        [Required] 
+        public string Name { get; set; }
+
+        public virtual ICollection<Event> Events { get; set; }
+        //public virtual ICollection<User> Users { get; set; }
     }
 }
