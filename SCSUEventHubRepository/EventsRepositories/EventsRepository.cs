58d6a627f179cd5e74846d45dd390dfc807fb5ac
edwardiscsu <ised03001@stcloudstate.edu>
2015-04-24 21:28:00 -0500

    Updated events repository

--- SCSUEventHubRepository/EventsRepositories/EventsRepository.cs
+++ SCSUEventHubRepository/EventsRepositories/EventsRepository.cs
@@ -9,7 +9,7 @@ using SCSUEventHubCommon.Tools;
 
 namespace SCSUEventHubRepository.EventsRepositories
 {
-    public class EventsRepository : IEventsRepository
+    public class EventsRepository : ContextDisposableRespository, IEventsRepository
     {
         public int? AddEvent(int adminID, Event newEvent)
         {
@@ -17,18 +17,10 @@ namespace SCSUEventHubRepository.EventsRepositories
 
             try
             {
-                var eventHubDB = new Entity.EventHubDBEntities();
-                AppDomain.CurrentDomain.SetData("DataDirectory",
-                    PathFactory.DatabasePath());
-
                 if (null != newEvent)
                 {
-                    //var eEvent = MapEventToEntity(newEvent);
-                    //eventHubDB.Events.Add(eEvent);
-                    //eventHubDB.SaveChanges();
-                    //newEventID = eEvent.ID;
-                    eventHubDB.Events.Add(newEvent);
-                    eventHubDB.SaveChanges();
+                    DBContext.Events.Add(newEvent);
+                    DBContext.SaveChanges();
                     newEventID = newEvent.ID;
                 }
                 else newEventID = new int?();
@@ -44,62 +36,211 @@ namespace SCSUEventHubRepository.EventsRepositories
 
         public Event GetEvent(int eventID)
         {
-            throw new NotImplementedException();
+            var dEvent = new Event();
+
+            try
+            {
+                dEvent = DBContext.Events
+                    .Where(e => e.ID == eventID).FirstOrDefault();
+            }
+            catch (Exception e)
+            {
+                dEvent = null;
+                throw new Exception(e.Message);
+            }
+
+            return dEvent;
         }
 
         public List<Event> GetEvents(int? userID = null, int? categoryID = null, int? adminID = null)
         {
-            throw new NotImplementedException();
+            var dEvents = new List<Event>();
+
+            try
+            {
+                if (null != userID)
+                {
+                    var subscribedIds = DBContext.Subscriptions
+                        .Where(s => s.UserID == userID.Value).Select(s => s.EventID);
+                    dEvents = DBContext.Events
+                        .Where(e => subscribedIds.Contains(e.ID))
+                        .Where(e => (categoryID.HasValue ? e.CategoryID == categoryID.Value : e.ID == e.ID)
+                                    && (adminID.HasValue ? e.AdminID == adminID.Value : e.ID == e.ID))
+                                    .ToList();
+                }
+                else
+                {
+                    dEvents = DBContext.Events
+                        .Where(e => (categoryID.HasValue ? e.CategoryID == categoryID.Value : e.ID == e.ID)
+                                    && (adminID.HasValue ? e.AdminID == adminID.Value : e.ID == e.ID))
+                                    .ToList();
+                }
+            }
+            catch (Exception e)
+            {
+                dEvents = null;
+                throw new Exception(e.Message);
+            }
+
+            return dEvents;
         }
 
         public int? UpdateEvent(Event updatedEvent)
         {
-            throw new NotImplementedException();
+            var updatedEventID = new int?();
+
+            try
+            {
+                if (null != updatedEvent)
+                {
+
+                }
+                else updatedEventID = null;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return updatedEventID;
         }
 
         public bool DeleteEvent(int eventID)
         {
-            throw new NotImplementedException();
+            var success = false;
+
+            try
+            {
+
+                success = true;
+            }
+            catch (Exception e)
+            {
+                success = false;
+                throw new Exception(e.Message);
+            }
+
+            return success;
         }
 
         public Subscription SubscribeToEvent(int userID, int eventID, ICollection<Reminder> reminders)
         {
-            throw new NotImplementedException();
+            var newSubscription = new Subscription();
+
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return newSubscription;
         }
 
         public bool UnsubscribeFromEvent(int userID, int eventID)
         {
-            throw new NotImplementedException();
+            var success = false;
+
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return success;
         }
 
         public Subscription GetSubscription(int eventID)
         {
-            throw new NotImplementedException();
+            var subscription = new Subscription();
+
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return subscription;
         }
 
         public int? AddReminder(int userID, int eventID, Reminder reminder)
         {
-            throw new NotImplementedException();
+            var reminderID = new int?();
+
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return reminderID;
         }
 
         public Reminder GetReminder(int reminderID)
         {
-            throw new NotImplementedException();
+            var reminder = new Reminder();
+
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return reminder;
         }
 
         public List<Reminder> GetReminders(int eventID)
         {
-            throw new NotImplementedException();
+            var reminders = new List<Reminder>();
+
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return reminders;
         }
 
         public int? UpdateReminder(int reminderID, Reminder reminder)
         {
-            throw new NotImplementedException();
+            var updatedReminderID = new int?();
+
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return updatedReminderID;
         }
 
         public bool DeleteReminder(int reminderID)
         {
-            throw new NotImplementedException();
+            var success = false;
+
+            try
+            {
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+
+            return success;
         }
         /*
 
