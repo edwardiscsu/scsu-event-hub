a69ed377bfc0317c0b80c8c536e7f82e23a0e8ac
edwardiscsu <ised03001@stcloudstate.edu>
2015-04-24 22:34:46 -0500

    Added initial app frame

--- SCSUEventHubClient/App_Start/BundleConfig.cs
+++ SCSUEventHubClient/App_Start/BundleConfig.cs
@@ -23,7 +23,13 @@ namespace SCSUEventHubClient
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                        "~/Content/bootstrap.css",
+                        "~/Content/site.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
