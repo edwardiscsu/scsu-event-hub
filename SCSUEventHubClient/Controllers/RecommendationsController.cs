2a1aceacf1d355abb52af1d78027da19f9e88c92
edwardiscsu <ised03001@stcloudstate.edu>
2015-04-26 17:28:21 -0500

    Implement Get Events

---
+++ SCSUEventHubClient/Controllers/RecommendationsController.cs
@@ -0,0 +1,26 @@
+﻿using SCSUEventHubRepository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCSUEventHubClient.Controllers
+{
+    public class RecommendationsController : Controller
+    {
+        private IRecommendationsRepository reccomendationsRepository;
+
+        public RecommendationsController(IRecommendationsRepository reccomendationsRepository)
+        {
+            this.reccomendationsRepository = reccomendationsRepository;
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
