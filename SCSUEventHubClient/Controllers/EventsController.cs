44ce19b04dc4dfd529f606b2e8cca82343f073ea
edwardiscsu <ised03001@stcloudstate.edu>
2015-04-26 21:45:33 -0500

    Re-added the events controller

---
+++ SCSUEventHubClient/Controllers/EventsController.cs
@@ -0,0 +1,44 @@
+﻿using SCSUEventHubModels.Models;
+using SCSUEventHubRepository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCSUEventHubClient.Controllers
+{
+    public class EventsController : Controller
+    {
+        private IEventsRepository eventsRepository;
+
+        public EventsController(IEventsRepository eventsRepository)
+        {
+            this.eventsRepository = eventsRepository;
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        // GET: Events/Get/?userID=1&categoryID=2&adminID=3
+        public List<Event> Get(int? userID = null, int? categoryID = null, int? adminID = null)
+        {
+            var events = new List<Event>();
+
+            try
+            {
+                events = eventsRepository.GetEvents(userID, categoryID, adminID);
+            }
+            catch (Exception e)
+            {
+                throw new HttpException(e.Message);
+            }
+
+            return events;
+        }
+    }
+}
---
+++ SCSUEventHubClient/Controllers/UsersController.cs
@@ -0,0 +1,26 @@
+﻿using SCSUEventHubRepository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SCSUEventHubClient.Controllers
+{
+    public class UsersController : Controller
+    {
+        private IUsersRepository usersRepository;
+
+        public UsersController(IUsersRepository usersRepository)
+        {
+            this.usersRepository = usersRepository;
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
