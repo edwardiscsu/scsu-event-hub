fe2a5d25417264176f45c9289153cc3ea98e6fa1
edwardiscsu <ised03001@stcloudstate.edu>
2015-04-27 17:58:36 -0500

    Use fake repo for now for events. Added initial eventservice ko codes

--- SCSUEventHubClient/Scripts/Service/EventService.js
+++ SCSUEventHubClient/Scripts/Service/EventService.js
@@ -1,3 +1,34 @@
-﻿function EventService(baseUrl) {
-    this.baseUrl = baseUrl;
-};
+﻿
+$(document).ready(function () {
+
+    function EventsViewModel() {
+        var self = this;
+
+        self.events = ko.observableArray([]);
+
+        self.refreshEvents = function () {
+            $.ajax({
+                type: 'GET',
+                url: 'http://localhost:63188/events/get/',
+                contentType: 'application/json; charset=utf-8',
+                dataType: "json",
+                beforeSend: function (xhr) {
+                    
+                },
+                success: function (data) {
+
+                },
+                error: function (jqXHR, status, error) {
+
+                },
+                complete: function () {
+
+                }
+            });
+        };
+        self.refreshEvents(); //Initial values
+    }
+
+    ko.applyBindings(new EventsViewModel());
+
+});
