cb8e1b963c58dceec41587c6b3a960b7df947a96
Harlan Sang <harlansang@gmail.com>
2015-04-26 23:10:54 -0500

    Resync Commit

---
+++ SCSUEventHubClient/Scripts/UserInterface/AppMenuShelf.js
@@ -0,0 +1,36 @@
+﻿function AppMenuShelf(appMenuId, openButtonId) {
+    this.appMenuId = appMenuId;
+    this.openButtonId = openButtonId;
+    this.isOpen = false;
+
+    $('#' + this.openButtonId).on("click", $.proxy(this.handleClickEvent, this));
+};
+
+AppMenuShelf.prototype.open = function () {
+    var originalThis = this;
+    $("#" + this.appMenuId).animate({
+        width: "100%",
+        opacity: 1.0
+    }, 2000, function () {
+        originalThis.isOpen = true;
+    });
+}
+
+AppMenuShelf.prototype.close = function () {
+    var originalThis = this;
+    $("#" + this.appMenuId).animate({
+        width: "0%",
+        opacity: 0.5
+    }, 2000, function () {
+        originalThis.isOpen = false;
+    });
+}
+
+AppMenuShelf.prototype.handleClickEvent = function () {
+    if (this.isOpen) {
+        this.close();
+    }
+    else {
+        this.open();
+    }
+}
