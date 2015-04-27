7312ddf874f670c873dc0d9cc57d5f3586faf959
edwardiscsu <ised03001@stcloudstate.edu>
2015-04-27 16:40:11 -0500

    updated events seed

--- SCSUEventHubRepository/Migrations/Configuration.cs
+++ SCSUEventHubRepository/Migrations/Configuration.cs
@@ -86,9 +86,61 @@ namespace SCSUEventHubRepository.Migrations
             context.SaveChanges();
 
             Event[] events = new Event[] {
-                new Event { CategoryID = categories[0].ID, EventName = "Hockey with Santa 1", DateTime = new DateTime(2015,5,6), Description = "play some hockey with Santa! It's awesome!" },
-                new Event { CategoryID = categories[0].ID, EventName = "Hockey with Santa 2", DateTime = new DateTime(2015,5,6), Description = "play some hockey with Santa! It's awesome!" },
-                new Event { CategoryID = categories[0].ID, EventName = "Hockey with Santa 3", DateTime = new DateTime(2015,5,6), Description = "play some hockey with Santa! It's awesome!" }
+                //Athletics
+                new Event { CategoryID = categories[0].ID, EventName = "Hockey with Santa", DateTime = new DateTime(2015,5,6), ImageURL = "pic1.jpeg", Description = "Can you beat santa in ice hockey? Prove yourselves!" },
+                new Event { CategoryID = categories[0].ID, EventName = "Atwood Game Night", DateTime = new DateTime(2015,6,1), ImageURL = "pic2.jpeg", Description = "Play pools and bowlings for free! Bring your friends!" },
+                new Event { CategoryID = categories[0].ID, EventName = "Joust Your Friend", DateTime = new DateTime(2015,7,9), ImageURL = "pic3.jpeg", Description = "Learn a" },
+
+                //Conferences & Workshops
+                new Event { CategoryID = categories[1].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,6,2), ImageURL = "pic1.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[1].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,6,8), ImageURL = "pic2.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[1].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,6,9), ImageURL = "pic3.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[1].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,7,2), ImageURL = "pic4.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[1].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,8,1), ImageURL = "pic5.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+
+                //Cultural
+                new Event { CategoryID = categories[2].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,5,2), ImageURL = "pic1.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[2].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,5,6), ImageURL = "pic2.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[2].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,5,9), ImageURL = "pic3.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+
+                //Entertainment
+                new Event { CategoryID = categories[3].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,5,6), ImageURL = "pic1.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[3].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,5,8), ImageURL = "pic2.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[3].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,6,1), ImageURL = "pic3.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[3].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,6,2), ImageURL = "pic4.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[3].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,7,2), ImageURL = "pic5.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[3].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,8,3), ImageURL = "pic1.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[3].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,8,2), ImageURL = "pic2.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[3].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,7,7), ImageURL = "pic3.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[3].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,6,4), ImageURL = "pic4.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[3].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,6,5), ImageURL = "pic5.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+
+                //Fine Arts
+                new Event { CategoryID = categories[4].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,5,7), ImageURL = "pic1.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[4].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,6,8), ImageURL = "pic2.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[4].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,7,2), ImageURL = "pic3.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[4].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,7,3), ImageURL = "pic4.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[4].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,8,4), ImageURL = "pic5.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+
+                //Recreation
+                new Event { CategoryID = categories[5].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,5,6), ImageURL = "pic1.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[5].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,6,6), ImageURL = "pic2.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[5].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,6,7), ImageURL = "pic3.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[5].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,7,1), ImageURL = "pic4.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[5].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,7,3), ImageURL = "pic5.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[5].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,7,5), ImageURL = "pic1.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+
+                //Speakers
+                new Event { CategoryID = categories[6].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,5,4), ImageURL = "pic1.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[6].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,5,9), ImageURL = "pic2.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[6].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,6,2), ImageURL = "pic3.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+
+                //Special Events
+                new Event { CategoryID = categories[7].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,5,6), ImageURL = "pic1.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[7].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,5,8), ImageURL = "pic2.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[7].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,6,1), ImageURL = "pic3.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[7].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,7,7), ImageURL = "pic4.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." },
+                new Event { CategoryID = categories[7].ID, EventName = "History Night: Fu-Go", DateTime = new DateTime(2015,8,1), ImageURL = "pic5.jpeg", Description = "Learn about Fu-Go. The Japanese secret weapon in WWII." }
             };
 
             context.Events.AddOrUpdate(
