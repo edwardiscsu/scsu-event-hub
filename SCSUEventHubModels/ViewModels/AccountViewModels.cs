d6ebe2f746bcd5671211f9af7663bc95d33918ff
Harlan Sang <harlansang@gmail.com>
2015-04-27 02:25:24 -0500

    Added Identity Support to Client

--- SCSUEventHubModels/ViewModels/AccountViewModels.cs
+++ SCSUEventHubModels/ViewModels/AccountViewModels.cs
@@ -109,4 +109,40 @@ namespace SCSUEventHubModels.ViewModels
         [Display(Name = "Email")]
         public string Email { get; set; }
     }
+	
+    public class ExternalLoginViewModel
+    {
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string State { get; set; }
+    }
+
+    public class ManageInfoViewModel
+    {
+        public string LocalLoginProvider { get; set; }
+
+        public string Email { get; set; }
+
+        public IEnumerable<UserLoginInfoViewModel> Logins { get; set; }
+
+        public IEnumerable<ExternalLoginViewModel> ExternalLoginProviders { get; set; }
+    }
+
+    public class UserInfoViewModel
+    {
+        public string Email { get; set; }
+
+        public bool HasRegistered { get; set; }
+
+        public string LoginProvider { get; set; }
+    }
+
+    public class UserLoginInfoViewModel
+    {
+        public string LoginProvider { get; set; }
+
+        public string ProviderKey { get; set; }
+    }
 }
